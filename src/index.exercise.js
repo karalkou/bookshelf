1414f68b5e9345447167428ed9262b6d6db662ad
Yury Karalkou <yury.karalkou@gmail.com>
2021-08-20 17:12:03 +0300

    No-ticket: Start 'Create a LoginForm Component' task
    1. created LoginForm component
    2. used it inside of the App

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import { Dialog } from '@reach/dialog'
 
@@ -6,8 +6,39 @@ import { Logo } from './components/logo'
 
 import '@reach/dialog/styles.css'
 
+function LoginForm({ onSubmit, buttonText }) {
+  const [userName, setUserName] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleUsrChange = e => setUserName(e.target.value)
+  const handlePswChange = e => setPassword(e.target.value)
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    onSubmit({ userName, password })
+  }
+
+  return (
+    <div>
+      <div>
+        <input type='text' value={userName} onChange={handleUsrChange} />
+      </div>
+      <div>
+        <input type='text' value={password} onChange={handlePswChange} />
+      </div>
+      <button onClick={handleSubmit}>{buttonText}</button>
+    </div>
+  )
+}
+
 function App() {
   const [openModal, setOpenModal] = React.useState('none')
+  const handleLoginSubmit = (formData) => {
+    console.log('login', formData)
+  }
+  const handleRegisterSubmit = (formData) => {
+    console.log('login', formData)
+  }
 
   return (
     <div>
@@ -25,6 +56,7 @@ function App() {
           <button onClick={() => setOpenModal('none')}>Close</button>
         </div>
         <h3>Login</h3>
+        <LoginForm onSubmit={handleLoginSubmit} buttonText='Login' />
       </Dialog>
 
       <Dialog aria-label='Registration form' isOpen={openModal === 'register'}>
@@ -32,6 +64,7 @@ function App() {
           <button onClick={() => setOpenModal('none')}>Close</button>
         </div>
         <h3>Register</h3>
+        <LoginForm onSubmit={handleRegisterSubmit} buttonText='Register' />
       </Dialog>
     </div>
   )
