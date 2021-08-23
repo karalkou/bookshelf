f52614c3e3f31c15afe4b546134b74bc0ed7ea63
Yury Karalkou <yury.karalkou@gmail.com>
2021-08-23 22:30:16 +0300

    No-ticket: Use useAsync (to enhance our UI with request statuses)

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -5,37 +5,73 @@ import * as React from 'react'
 import * as auth from 'auth-provider'
 import { AuthenticatedApp } from './authenticated-app'
 import { UnauthenticatedApp } from './unauthenticated-app'
+import { client } from './utils/api-client'
+import { useAsync } from './utils/hooks'
+import { FullPageSpinner } from './components/lib'
+
+import * as colors from './styles/colors'
 
 async function getUser() {
   let user = null
   const token = await auth.getToken()
 
   if (token) {
-    user = await client('me', { token })
+    const data = await client('me', { token })
+    user = data.user
   }
 
   return user
 }
 
 function App() {
-  const [user, setUser] = React.useState(null)
+  const {
+    data: user,
+    setData,
+    run,
+    error,
+    isLoading,
+    isIdle,
+    isError,
+    isSuccess,
+  } = useAsync()
 
   React.useEffect(() => {
-    getUser()
-      .then(data => setUser(data.user))
-  }, [])
+    run(getUser())
+  }, [run])
 
-  const login = form => auth.login(form).then(u => setUser(u))
-  const register = form => auth.register(form).then(u => setUser(u))
+  const login = form => auth.login(form).then(user => setData(user))
+  const register = form => auth.register(form).then(user => setData(user))
   const logout = () => {
     auth.logout()
-    setUser(null)
+    setData(null)
   }
 
-  return user ?
-    <AuthenticatedApp user={user} logout={logout} />
-    :
-    <UnauthenticatedApp login={login} register={register} />
+  return <>
+    {(isLoading || isIdle) && <FullPageSpinner />}
+    {isError && (
+      <div
+        css={{
+          color: colors.danger,
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <p>Uh oh... There's a problem. Try refreshing the app.</p>
+        <pre>{error.message}</pre>
+      </div>
+    )}
+    {
+      isSuccess && (
+        user ?
+          <AuthenticatedApp user={user} logout={logout} />
+          :
+          <UnauthenticatedApp login={login} register={register} />
+      )
+    }
+  </>
 }
 
 export { App }
