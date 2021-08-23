4ef25a87aaaaa85e84ad9daa6ae751f8ab64abeb
Yury Karalkou <yury.karalkou@gmail.com>
2021-08-23 21:37:53 +0300

    No-ticket: Send a request to get info of previously authenticated user
    Added a check whether we were authenticated earlier and if so we send a request to get a user information

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -6,8 +6,24 @@ import * as auth from 'auth-provider'
 import { AuthenticatedApp } from './authenticated-app'
 import { UnauthenticatedApp } from './unauthenticated-app'
 
+async function getUser() {
+  let user = null
+  const token = await auth.getToken()
+
+  if (token) {
+    user = await client('me', { token })
+  }
+
+  return user
+}
+
 function App() {
   const [user, setUser] = React.useState(null)
+
+  React.useEffect(() => {
+    getUser()
+      .then(data => setUser(data.user))
+  }, [])
 
   const login = form => auth.login(form).then(u => setUser(u))
   const register = form => auth.register(form).then(u => setUser(u))
