081796b226b341146968bd15cbb6badb7020f798
Yury Karalkou <yury.karalkou@gmail.com>
2021-09-06 16:09:22 +0300

    No-ticket: Create an AuthProvider Component
    1. AuthProvider component is created
    2. root component and app component are refactored due to changes mentioned above

--- src/context/auth-context.exercise.js
+++ src/context/auth-context.exercise.js
@@ -1,11 +1,67 @@
-import {createContext, useContext} from 'react'
+import React, {createContext, useContext} from 'react'
+import * as auth from 'auth-provider'
+import {FullPageSpinner, FullPageErrorFallback} from '../components/lib'
+import {client} from '../utils/api-client'
+import {useAsync} from '../utils/hooks'
 
-export const AuthContext = createContext()
+const AuthContext = createContext()
+AuthContext.displayName = 'AuthContext'
 
-export function useAuth() {
+async function getUser() {
+  let user = null
+
+  const token = await auth.getToken()
+  if (token) {
+    const data = await client('me', {token})
+    user = data.user
+  }
+
+  return user
+}
+
+function useAuth() {
   const context = useContext(AuthContext)
   if (context === undefined) {
-    throw new Error(`useAuth must be used within an AuthContext provider`)
+    throw new Error(`useAuth must be used within an AuthProvider`)
   }
   return context
 }
+
+function AuthProvider(props) {
+  const {
+    data: user,
+    error,
+    isLoading,
+    isIdle,
+    isError,
+    isSuccess,
+    run,
+    setData,
+  } = useAsync()
+
+  React.useEffect(() => {
+    run(getUser())
+  }, [run])
+
+  const login = form => auth.login(form).then(user => setData(user))
+  const register = form => auth.register(form).then(user => setData(user))
+  const logout = () => {
+    auth.logout()
+    setData(null)
+  }
+
+  if (isLoading || isIdle) {
+    return <FullPageSpinner />
+  }
+
+  if (isError) {
+    return <FullPageErrorFallback error={error} />
+  }
+
+  if (isSuccess) {
+    const value = {user, login, register, logout}
+    return <AuthContext.Provider value={value} {...props} />
+  }
+}
+
+export {useAuth, AuthProvider}
